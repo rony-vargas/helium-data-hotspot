7fe3e8c261d5f32928d24e2ea7e6d6fe46289e13
Rony A. Vargas <rony@vnovait.net>
2022-02-02 21:04:36 -0500

    FIX start-gatewayrs.sh after ALPHA-21

--- hm-gatewayrs/start-gatewayrs.sh
+++ hm-gatewayrs/start-gatewayrs.sh
@@ -24,14 +24,27 @@ then
   echo "Key file already exists"
   echo 'keypair = "/var/data/gateway_key.bin"' >> settings.toml
 else
-  echo "Copying key file to persistent storage"
+  echo "Starting the service temporarily, copy key to persistent storage and restart server"
+
+  #Addition to Alpha21 change
+  /usr/bin/helium_gateway -c /etc/helium_gateway server &
+
+  echo "Test line blocking"
+  sleep 30
+  #cp /etc/helium_gateway/gateway_key.bin /var/data/gateway_key.bin
+  #End Addition to Alpha21 Change
+
   if ! PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway key info)
   then
-    echo "Can't get miner key info"
+    echo "Can't get miner key info, possible running first time?"
+    #balena-idle #to avoid crashing and allow debug
     exit 1
   else
     cp /etc/helium_gateway/gateway_key.bin /var/data/gateway_key.bin
     echo 'keypair = "/var/data/gateway_key.bin"' >> settings.toml
+    PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway key info) || exit 1
+    echo "$PUBLIC_KEYS" > /var/data/key_json
+    python3 /opt/nebra-gatewayrs/keys.py
   fi
 fi
 
@@ -39,14 +52,19 @@ echo "" >> settings.toml
 cat /etc/helium_gateway/settings.toml.template >> settings.toml
 cp settings.toml /etc/helium_gateway/settings.toml
 
-if ! PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway key info)
+#addition after alpha21
+echo "kill temp server"
+kill %1 2> /dev/null #Killing job when running the first time the helium-miner, before key provisioning, avoiding collision with a new instance
+if [ -f "/var/data/key_json" ]
+#if ! PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway key info)
 then
-  echo "Can't get miner key info"
-  exit 1
+  echo "Getting Public Key from existing file"
+  PUBLIC_KEYS=$(cat /var/data/key_json)
+  #balena-idle #to avoid crahing and allow debug
 else
-  echo "$PUBLIC_KEYS" > /var/data/key_json
+  echo "Unable to get the Public Key from existing file"
+  #PUBLIC_KEYS=$(/usr/bin/helium_gateway -c /etc/helium_gateway key info) || exit 1
+  #echo "$PUBLIC_KEYS" > /var/data/key_json
+  #python3 /opt/nebra-gatewayrs/keys.py
 fi
-
-python3 /opt/nebra-gatewayrs/keys.py
-
 /usr/bin/helium_gateway -c /etc/helium_gateway server
